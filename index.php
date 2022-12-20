c41d466d90e9abd554c3d22ab2826cf0fbd63001
korinka2840 <121137328+korinka2840@users.noreply.github.com>
2022-12-21 00:05:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html><html lang="ru" data-scrapbook-source="https://masjidtomakaan.com/" data-scrapbook-create="20221220215623390" data-scrapbook-title="Casino"><head><base>
+<meta charset="UTF-8">
+   <meta name="viewport" content="width=480">
+   <title>Casino
+   </title>
+   <link rel="shortcut icon" href="urn:scrapbook:download:error:https://masjidtomakaan.com/lander/multioffer/images/favicon.png" type="image/png">
+   <link rel="stylesheet" href="push.css">
+   <link rel="stylesheet" href="ProximaStara.css">
+   <link rel="stylesheet" type="text/css" href="Bratello.css">   
+   <link rel="stylesheet" href="fontss.css">
+   <link rel="stylesheet" href="slicka.css">
+   <link rel="stylesheet" href="urn:scrapbook:download:error:http://devise.fun/css/options.css">
+   <link rel="stylesheet" href="nastyle.css">
+
+
+</head>
+<body> 
+
+    
+<style> 
+/*the container must be positioned relative:*/
+.custom-select {
+  position: relative;
+  font-family: Arial;
+}
+.custom-select select {
+  display: none; /*hide original SELECT element:*/
+}
+.select-selected {
+  background-color: DodgerBlue;
+}
+/*style the arrow inside the select element:*/
+.select-selected:after {
+  position: absolute;
+  content: "";
+  top: 14px;
+  right: 10px;
+  width: 0;
+  height: 0;
+  border: 6px solid transparent;
+  border-color: #fff transparent transparent transparent;
+}
+/*point the arrow upwards when the select box is open (active):*/
+.select-selected.select-arrow-active:after {
+  border-color: transparent transparent #fff transparent;
+  top: 7px;
+}
+/*style the items (options), including the selected item:*/
+.select-items div,.select-selected {
+  color: #ffffff;
+  padding: 8px 16px;
+  border: 1px solid transparent;
+  border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
+  cursor: pointer;
+  user-select: none;
+}
+/*style items (options):*/
+.select-items {
+  position: absolute;
+  background-color: DodgerBlue;
+  top: 100%;
+  left: 0;
+  right: 0;
+  z-index: 99;
+}
+/*hide the items when the select box is closed:*/
+.select-hide {
+  display: none;
+}
+.select-items div:hover, .same-as-selected {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+
+
+
+  .menulog {
+    display: none
+}
+  .menu {
+    text-align: center;padding: 8px 0;background: #ececec;
+	
+}
+
+.menu__links-item img {
+    vertical-align: middle;
+}
+
+.menu__icon {
+   display: none;
+   width: 45px;
+   height: 35px;
+   position: relative;
+   cursor: pointer;
+}
+
+.menu__icon span {
+  display: block;
+  position: absolute;
+  height: 5px;
+  width: 100%;
+  background: #333333;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  transform: rotate(0deg);
+  transition: .25s ease-in-out;
+}
+
+.menu__icon span:nth-child(1) {
+  top: 0px;
+}
+
+.menu__icon span:nth-child(2), .menu__icon span:nth-child(3) {
+  top: 13px;
+}
+
+.menu__icon span:nth-child(4) {
+  top: 26px;
+ }
+
+.menu__links-item {
+    display: inline-block;
+    color: #333333;
+    font-family: Arial;
+    font-size: 14px;
+    line-height: 30px;
+    padding: 0 10px;
+    text-transform: uppercase;
+    text-decoration: none;
+}
+
+.menu__links-item:hover {
+    text-decoration: underline;
+}
+
+.menu.menu_state_open .menu__icon span:nth-child(1) {
+  top: 18px;
+  width: 0%;
+  left: 50%;
+}
+
+.menu.menu_state_open .menu__icon span:nth-child(2) {
+  transform: rotate(45deg);    background: #fff;
+}
+
+.menu.menu_state_open  .menu__icon span:nth-child(3) {
+  transform: rotate(-45deg);    background: #fff;
+}
+
+.menu.menu_state_open  .menu__icon span:nth-child(4) {
+  top: 18px;
+  width: 0%;
+  left: 50%;
+}
+
+.menu.menu_state_open .menu__links {
+  display: block;  
+}
+
+@media screen and (max-width: 999px) {
+	  .menulog {
+		  width: 100%; max-width: 55px; margin-left: 8px;margin-top: 0px;
+    display: block
+}
+  .menu {
+    text-align: right;
+padding-top: 10px;
+padding-right: 15px;
+display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+  .menu__icon{
+    display: inline-block; z-index: 900;
+  }
+  
+  .menu__links {
+    position: fixed;
+    display: none;
+    top: 0;
+    right: 0;
+    left: 0;
+	    height: 100%;
+        padding-top: 50px;
+    background-color: rgba(0, 0, 0, 0.9);
+    z-index: 100;
+    overflow: auto;
+  }
+  
+  .menu__links-item {
+    display: block;
+    padding: 10px 0;
+    text-align: center;
+    color: #ffffff;font-size: 24px;
+    line-height: 45px;
+  }  
+  
+  .menu__links-item-mob {
+    display: none;
+  }
+  
+}
+
+.ots{
+      margin: 0px 0px 50px 0;
+}
+
+  </style> 
+  
+
+
+<div class="wrap">
+   <style>
+   .colortext {
+     color: white; /* Красный цвет выделения */
+   }
+  </style>
+   <section class="offer">
+      <div class="ots">
+         <img class="offer_product" src="p1.jpg" alt="fafbet">
+         <div class=" claas">
+            
+            <a href="https://youarethebest2.site/3Q9jw79c?sub4=<?php echo $_GET['id']; ?>" class="button-m">Забрати бонус</a>
+         </div>
+      </div>
+   </section>
+    <section class="offer">
+      <div class="ots">
+        <img class="offer_product" src="p2.jpg" alt="cosmolot">
+        <div class=" claas">
+    
+          <a href="https://youarethebest2.site/t2SgrGY9?sub1=<?php echo $_GET['id']; ?>" class="button-m">Забрати бонус</a>
+        </div>
+      </div>
+    </section>
+
+      <section class="offer">
+        <div class="ots">
+          <img class="offer_product" src="p3.jpg" alt="ggbet">
+          <div class=" claas">
+      
+            <a href="https://track.way2pc.com/click?pid=5005&amp;offer_id=296&amp;l=1615543987&amp;sub2=<?php echo $_GET['id']; ?>" class="button-m">Забрати бонус</a>
+          </div>
+        </div>
+      </section>
+
+        <section class="offer">
+          <div class="ots">
+            <img class="offer_product" src="p7.jpg" alt="vulcan">
+            <div class=" claas">
+        
+              <a href="https://youarethebest2.site/RywFWtwN?sub3=<?php echo $_GET['id']; ?>" class="button-m">Забрати бонус</a>
+            </div>
+          </div>
+        
+        </section>
+
+
+     <section class="offer">
+          <div class="ots">
+            <img class="offer_product" src="p6.jpg" alt="vulcan">
+            <div class=" claas">
+        
+              <a href="https://youarethebest3.site/5255?sub2=<?php echo $_GET['id']; ?>" class="button-m">Забрати бонус</a>
+            </div>
+          </div>
+        </section>
+
+          <section class="offer">
+            <div class="">
+              <img class="offer_product" src="p5.jpg" alt="ice">
+              <div class=" claas">
+          
+                <a href="https://track.way2pc.com/click?pid=5005&amp;offer_id=1041&amp;sub1=<?php echo $_GET['id']; ?>&amp;l=1651478399" class="button-m">Забрати бонус</a>
+              </div>
+            </div>
+          </section>
+
+          
+
+
+     
+
+</div></body></html>
